3652b87e4978c6461e362990413a16259e936a96
Will <wp.neversummer@gmail.com>
2013-10-06 00:05:40 -0400

    implement load image

--- WPF_Image_Editor/MainWindow.xaml.cs
+++ WPF_Image_Editor/MainWindow.xaml.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -8,6 +14,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -20,9 +27,106 @@ namespace WPF_Image_Editor
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Bitmap originalPicture;
+        private Bitmap currentPicture;
+
+        private ColorMatrix greyscaleConMatrix = new ColorMatrix(
+            new float[][]
+            {
+                new float[] {.22f,.22f,.22f, 0, 0},
+                new float[] {.59f,.59f,.59f, 0, 0},
+                new float[] {.11f,.11f,.11f, 0, 0},
+                new float[] {  0,   0,   0,  1, 0},
+                new float[] {  0,   0,   0,  0, 1}
+            });
+
+        private ColorMatrix invertConMatrix = new ColorMatrix(
+           new float[][]
+            {
+                new float[] { -1,   0,   0,  0,  1},
+                new float[] {  0,  -1,   0,  0,  1},
+                new float[] {  0,   0,  -1,  0,  1},
+                new float[] {  0,   0,   0,  1,  0},
+                new float[] {  1,   1,   1,  0,  1}
+            });
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void open_FileDialog(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(System.Environment.SpecialFolder.MyPictures);
+            openFileDialog.Filter = "JPEG Compressed Image (*.jpg)|*.jpg|GIF Image(*.gif)|*.gif|Bitmap Image(*.bmp)|*.bmp|PNG Image (*.png)|*.png";
+            openFileDialog.FilterIndex = 1;
+
+            Nullable<bool> result = openFileDialog.ShowDialog();
+
+            if (result == true)
+            {
+                originalPicture = new Bitmap(openFileDialog.FileName);
+                currentPicture = new Bitmap(openFileDialog.FileName);
+                setMainPicture(currentPicture);
+
+                //this.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void setMainPicture(Bitmap aBitmap)
+        {
+            mainImage.Source = BitmapToBitmapSource(aBitmap);
+        }
+
+
+        private Bitmap BitmapImage2Bitmap(BitmapImage bitmapImage)
+        {
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bitmapImage));
+                enc.Save(outStream);
+                System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(outStream);
+
+                // return bitmap; <-- leads to problems, stream is closed/closing ...
+                return new Bitmap(bitmap);
+            }
+        }
+
+
+        public static BitmapSource BitmapToBitmapSource(Bitmap source)
+        {
+            BitmapSource bitSrc = null;
+
+            var hBitmap = source.GetHbitmap();
+
+            try
+            {
+                bitSrc = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                    hBitmap,
+                    IntPtr.Zero,
+                    Int32Rect.Empty,
+                    BitmapSizeOptions.FromEmptyOptions());
+            }
+            catch (Win32Exception)
+            {
+                bitSrc = null;
+            }
+            finally
+            {
+                NativeMethods.DeleteObject(hBitmap);
+            }
+
+            return bitSrc;
+        }
+
+
+        internal static class NativeMethods
+        {
+            [DllImport("gdi32.dll")]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            internal static extern bool DeleteObject(IntPtr hObject);
         }
     }
 }
